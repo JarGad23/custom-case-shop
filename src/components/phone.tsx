9d5f6924f6f52f2c05c651e7065268c61500e8e3
JarGad23 <j.gad@m8b.pl>
2024-05-28 19:44:27 +0200

    Developed landing page with reusable components

---
+++ src/components/phone.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import { HTMLAttributes } from "react";
+
+interface Props extends HTMLAttributes<HTMLDivElement> {
+  imgSrc: string;
+  dark?: boolean;
+}
+
+export const Phone = ({ imgSrc, dark = false, className, ...props }: Props) => {
+  return (
+    <div
+      className={cn(
+        "relative pointer-events-none z-50 overflow-hidden",
+        className
+      )}
+      {...props}
+    >
+      <Image
+        src={
+          dark
+            ? "/phone-template-dark-edges.png"
+            : "/phone-template-white-edges.png"
+        }
+        width={300}
+        height={300}
+        className="pointer-events-none z-50 select-none"
+        alt="phone image"
+      />
+      <div className="absolute -z-10 inset-0">
+        <Image
+          src={imgSrc}
+          fill
+          alt="overlaying phone image"
+          className="object-cover"
+        />
+      </div>
+    </div>
+  );
+};
